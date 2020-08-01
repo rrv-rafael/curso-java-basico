85e30154e7c8faa174796afb193d904d1bdee2b2
Rafael Rodrigues <rrv.rafael@outlook.com>
2020-07-31 21:35:04 -0300

    add class

---
+++ eclipse/src/com/rrvrafael/cursojava/aula10/Variaveis.java
@@ -0,0 +1,18 @@
+package com.rrvrafael.cursojava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		int idade = 20;
+		String nome = "Rafael";
+		String nomeDoMeuCachorro = "Rex";
+		
+		idade = 25;
+		
+		System.out.println("Idade = " + idade);
+		System.out.println("Nome = " + nome);
+		System.out.println("Nome do meu cachorro = " + nomeDoMeuCachorro);
+	}
+
+}
