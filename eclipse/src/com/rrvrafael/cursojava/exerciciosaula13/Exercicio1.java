daca79d303b346240a858d0e4372364dc37646f1
Rafael Rodrigues <rrv.rafael@outlook.com>
2020-08-01 17:24:32 -0300

    new class of exercises

---
+++ eclipse/src/com/rrvrafael/cursojava/exerciciosaula13/Exercicio1.java
@@ -0,0 +1,10 @@
+package com.rrvrafael.cursojava.exerciciosaula13;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Alô Mundo!");
+	}
+
+}
---
+++ eclipse/src/com/rrvrafael/cursojava/exerciciosaula13/ExercicioDois.java
@@ -0,0 +1,10 @@
+package com.rrvrafael.cursojava.exerciciosaula13;
+
+public class ExercicioDois {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
