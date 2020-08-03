1acfd79586cf5f0de1ec5346841f72aa000254e3
Rafael Rodrigues <sistemas.puccamp@gmail.com>
2020-08-03 13:32:14 -0300

    update

--- intellij/src/com/rrvrafael/cursojava/exerciciosaula13/ExercicioQuatorze.java
+++ intellij/src/com/rrvrafael/cursojava/exerciciosaula13/ExercicioQuatorze.java
@@ -15,8 +15,17 @@ public class ExercicioQuatorze {
         System.out.println("Digite a velocidade de um link de internet (em Mbps): ");
         velLinkInt = scan.nextDouble();
 
+        tempAproximado = (velLinkInt / 8) * (tamArq * 1000000);
+        //1 megabit equivale a 125000 bytes
+
         //O tamanho do arquivo é de 10 megabytes
         //A velocidade de Mbps é de 100 megabits por segundo
         //Portanto, 1 byte equivale a 8 bits, ou seja, tenho que dividir 100 por 8 que são 12,5 bytes por segundo
+        //1 MB equivale a 1000000 de bytes
+        //No caso de 12,5 bytes por segundo, em um minuto são 750bytes baixados, em uma hora 45,000
+
+        //System.out.println("Tamanho do arquvo em bytes: " + tamArq * 1000000);
+        //System.out.println("Velocidade do link em bytes: " + velLinkInt / 8);
+        System.out.println("Tempo aproximado para o download: " + tempAproximado);
     }
 }
