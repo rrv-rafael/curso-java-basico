26a4eadfb579a8cb75d543376bd810cf5abd356c
Rafael Rodrigues <sistemas.puccamp@gmail.com>
2021-01-21 16:46:47 -0300

    Redone exercises

---
+++ intellij/src/com/rrvrafael/cursojava/redoneexercisesaula13/exercise14.java
@@ -0,0 +1,43 @@
+package com.rrvrafael.cursojava.redoneexercisesaula13;
+
+import java.util.Scanner;
+
+public class exercise14 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        double tamArquivo, velLinkInternet, tempoAproximado;
+
+        System.out.println("Informe o tamanho do arquivo para download (em MB)");
+        tamArquivo = scan.nextDouble();
+
+        System.out.println("Informe a velocidade do link de Internet (em Mbps):");
+        velLinkInternet = scan.nextDouble();
+
+        //Mbps é Megabits por segundo
+        //1 byte = 8 bits, divide Mbps por 8 que chega no MB por segundo
+
+        System.out.println("MB por segundo: " + velLinkInternet / 8);
+
+        tempoAproximado = tamArquivo / (velLinkInternet / 8);
+
+        System.out.println(tempoAproximado);
+
+        if (tempoAproximado < 60)
+        {
+            System.out.println("O tempo aproximado de download do arquivo usando este link é: " + tempoAproximado + " segundos");
+        }
+        else
+        {
+            tempoAproximado = tempoAproximado / 60;
+            System.out.println("O tempo aproximado de download do arquivo usando este link é: " + tempoAproximado + " minutos");
+        }
+
+
+        //
+
+
+    }
+}
