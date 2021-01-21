4c0a23322f2cfb6af78ac83bee0ca292c8a4b258
Rafael Rodrigues <sistemas.puccamp@gmail.com>
2021-01-21 09:11:59 -0300

    redone exercises
    Redone exercises from class 13

---
+++ intellij/src/com/rrvrafael/cursojava/redoneexercisesaula13/exercise01.java
@@ -0,0 +1,8 @@
+package com.rrvrafael.cursojava.redoneexercisesaula13;
+
+public class exercise01 {
+    public static void main(String[] args) {
+
+        System.out.println("Alô Mundo!");
+    }
+}
