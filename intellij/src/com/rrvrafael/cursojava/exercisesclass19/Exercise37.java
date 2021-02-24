575fd96551406ec5d195597ff4b6d4f7fa9d35f3
Rafael Rodrigues <sistemas.puccamp@gmail.com>
2021-02-24 15:41:47 -0300

    Exercises from class 19

---
+++ intellij/src/com/rrvrafael/cursojava/exercisesclass19/Exercise37.java
@@ -0,0 +1,33 @@
+package com.rrvrafael.cursojava.exercisesclass19;
+
+public class Exercise37 {
+
+    public static void main(String[] args) {
+
+        int[] vetorA = new int[15];
+        double[] vetorB = new double[15];
+
+        System.out.println("Elementos do vetor A:");
+
+        for (int i = 0; i < vetorA.length; i++)
+        {
+            vetorA[i] = i + 1;
+
+            System.out.print(vetorA[i] + " ");
+
+            vetorB[i] = vetorA[i];
+
+            for (int j = vetorA[i] - 1; j > 1; j--)
+            {
+                vetorB[i] *= j;
+            }
+        }
+
+        System.out.println("\n\nElementos do vetor B:");
+
+        for (double vetor: vetorB)
+        {
+            System.out.print(vetor + " ");
+        }
+    }
+}
