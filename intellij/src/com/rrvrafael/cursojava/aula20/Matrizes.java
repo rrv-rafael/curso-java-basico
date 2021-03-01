0bc8f0e635d3b7a4216e04e5fe63062d29cb4f4d
Rafael Rodrigues <rrv.rafael@outlook.com>
2021-03-01 18:17:38 -0300

    class 20

--- intellij/src/com/rrvrafael/cursojava/aula20/Matrizes.java
+++ intellij/src/com/rrvrafael/cursojava/aula20/Matrizes.java
@@ -1,16 +1,31 @@
 package com.rrvrafael.cursojava.aula20;
 
-import java.util.Random;
-import java.util.Scanner;
-
 public class Matrizes {
 
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-        Random random = new Random();
+        //Forma de inicializar matrizes e vetores de uma só vez:
 
-        double[][] notasAlunos = new double[30][4];
+        double[][] notasAlunosDois = {{10, 7, 8, 9.5}, {9, 8, 7, 9}};
+        double[] mediaAlunosDois = {8.625, 8.25};
+
+        for (int i = 0; i < notasAlunosDois.length; i++)
+        {
+            System.out.println("Matriz notasAlunosDois: " + (i + 1));
+            for (int j = 0; j < notasAlunosDois[i].length; j++)
+            {
+                System.out.print(notasAlunosDois[i][j] + " ");
+            }
+            System.out.println("\n");
+        }
+
+        for(int i = 0; i < mediaAlunosDois.length; i++)
+        {
+            System.out.println("Media do aluno " + (i + 1) + ": " + mediaAlunosDois[i] + "\n");
+        }
+
+        double[][] notasAlunos = new double[3][4];
+        double soma;
 
         notasAlunos[0][0] = 10;
         notasAlunos[0][1] = 7;
@@ -29,34 +44,27 @@ public class Matrizes {
 
         for (int i = 0; i < notasAlunos.length; i++)
         {
-            System.out.println("\nAluno " + (i + 1) + ":");
+            System.out.println("Aluno " + (i + 1) + ":");
 
             for (int j = 0; j < notasAlunos[i].length; j++)
             {
-                System.out.print("i: " + i + " ");
+                System.out.print(notasAlunos[i][j] + " ");
             }
+            System.out.println("\n");
         }
 
-
-        /*for (int i = 0; i < notasAlunos.length; i++)
-        {
-            for (int j = 0; j < 4; j++)
-            {
-                notasAlunos[i][j] = random.nextInt(10) + 1;
-                System.out.println("Digite a nota " + (j + 1) + " do aluno " + (i + 1) + ":");
-                notasAlunos[i][j] = scan.nextDouble();
-            }
-        }
+        notasAlunos[1][3] = 8;
 
         for (int i = 0; i < notasAlunos.length; i++)
         {
-            System.out.println("\n\nNotas do aluno " + (i + 1) + ":");
+            soma = 0;
 
-            for (int j = 0; j < 4; j++)
+            for (int j = 0; j < notasAlunos[i].length; j++)
             {
-                System.out.print(notasAlunos[i][j] + " ");
+                soma += notasAlunos[i][j];
             }
+
+            System.out.println("Média do aluno " + (i + 1) + ": " + (soma / notasAlunos[i].length));
         }
-        */
     }
 }
