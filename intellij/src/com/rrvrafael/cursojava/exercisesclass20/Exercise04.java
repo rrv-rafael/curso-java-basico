ebe3a641fbe7aab8fa4a475ba8f9379c189b3674
Rafael Rodrigues <rrv.rafael@outlook.com>
2021-03-05 00:32:36 -0300

    update

--- intellij/src/com/rrvrafael/cursojava/exercisesclass20/Exercise04.java
+++ intellij/src/com/rrvrafael/cursojava/exercisesclass20/Exercise04.java
@@ -9,10 +9,19 @@ public class Exercise04 {
         Scanner scan = new Scanner(System.in);
 
         String[][] agendaPessoal = new String[2][4];
-        int[] posicaoDia = new int[2];
-        int[] posicaoHorario = new int[4];
+        int[] posicaoDia = new int[8];
+        int[] posicaoHorario = new int[8];
         int diaMes, horario, count = 0;
         boolean continuar = true;
+
+        /*agendaPessoal[0][0] = "a";
+        agendaPessoal[0][1] = "b";
+        agendaPessoal[0][2] = "c";
+        agendaPessoal[0][3] = "d";
+        agendaPessoal[1][0] = "e";
+        agendaPessoal[1][1] = "f";
+        agendaPessoal[1][2] = "g";
+        agendaPessoal[1][3] = "h";*/
 
         System.out.println("Menu de opções - Digite de (1 - 30) para indicar o dia do mês e de (1 - 24) para indicar a hora que deseja alterar.");
         System.out.println("Para sair digite -1 na opção para indicar o mês.");
@@ -22,25 +31,21 @@ public class Exercise04 {
             System.out.println("Digite o dia do mês que deseja alterar:");
             diaMes = scan.nextInt();
 
-            if (diaMes == -1)
-            {
-                continuar = false;
-            }
-            else
+            if (diaMes >= 0)
             {
                 System.out.println("Digite o horário que deseja alterar:");
                 horario = scan.nextInt();
 
                 posicaoDia[count] = diaMes;
                 posicaoHorario[count] = horario;
-
-                //posicaoDia[count] = diaMes - 1;
-                //posicaoHorario[count] = horario - 1;
                 count++;
 
                 System.out.println("Agora, informe o compromisso:");
                 agendaPessoal[diaMes][horario] = scan.next();
-                //agendaPessoal[diaMes - 1][horario - 1] = scan.next();
+            }
+            else
+            {
+                continuar = false;
             }
         }
         while (continuar);
@@ -48,13 +53,12 @@ public class Exercise04 {
         for (int i = 0; i < agendaPessoal.length; i++)
         {
             System.out.println("Compromissos do dia " + posicaoDia[i] + " :");
+
             for (int j = 0; j < agendaPessoal[i].length; j++)
             {
-                System.out.println("Comprimisso do horário " + posicaoHorario[j] + " :");
-
-                if (i == posicaoDia[i] && j == posicaoHorario[j])
+                if (agendaPessoal[i][j] != null)
                 {
-                    System.out.print("Compromisso: " + agendaPessoal[i][j]);
+                    System.out.println(agendaPessoal[i][j]);
                 }
             }
         }
