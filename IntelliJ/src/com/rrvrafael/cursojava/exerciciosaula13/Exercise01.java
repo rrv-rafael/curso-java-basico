5f68b827b2fc316a04c1123d4cb913a1ee9e18be
Rafael Rodrigues <sistemas.puccamp@gmail.com>
2021-03-15 16:16:59 -0300

    update

--- IntelliJ/src/com/rrvrafael/cursojava/exerciciosaula13/Exercise01.java
+++
@@ -1,8 +0,0 @@
-package com.rrvrafael.cursojava.redoneexercisesaula13;
-
-public class Exercise01 {
-    public static void main(String[] args) {
-
-        System.out.println("Alô Mundo!");
-    }
-}
